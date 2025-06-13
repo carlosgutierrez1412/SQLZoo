73616f4793f22b4587b8203527a09c1c9683b911
Carlos Alberto Gutierrez <carlosgutierrez141201@gmail.com>
2025-06-12 21:38:09 -0400

    Update Help Desk Easy Questions.sql

--- Assessments/Help Desk/Help Desk Easy Questions.sql
+++ Assessments/Help Desk/Help Desk Easy Questions.sql
@@ -5,3 +5,19 @@ FROM Issue
 WHERE detail LIKE '%index%' AND
 detail LIKE '%Oracle%'
 
+-- 2. Samantha Hall made three calls on 2017-08-14. Show the date and time for each
+SELECT call_date,
+first_name, last_name
+FROM Caller ca
+JOIN Issue i
+ON ca.caller_id = i.caller_id
+WHERE first_name = 'Samantha' AND last_name = 'Hall'
+ORDER BY call_date
+LIMIT 3
+
+-- 3. There are 500 calls in the system (roughly). Write a query that shows the number that have each status.
+SELECT status,
+COUNT(*) AS volume
+FROM Issue
+GROUP BY status
+
